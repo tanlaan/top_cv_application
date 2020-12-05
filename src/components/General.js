c91e694be3ea16826536a3989588f16757397b51
Chris Bolas <tanlaan33@gmail.com>
2020-12-05 00:18:42 -0800

    Refactored input change handlers.

--- src/components/General.js
+++ src/components/General.js
@@ -16,36 +16,18 @@ class GeneralEdit extends Component {
                 phone: '',
             } 
         }
-
-        this.handleNameChange = this.handleNameChange.bind(this)
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handlePhoneChange = this.handlePhoneChange.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
-    handleNameChange(e) {
+
+    handleInputChange(e, valueName) {
         this.setState( (prevState) => {
-            return {
-                ...prevState,
-                name: e.target.value
-            }
+            let newState = {...prevState}
+            newState[valueName] = e.target.value
+            return newState
         })
     }
-    handleEmailChange(e) {
-        this.setState( (prevState) => {
-            return {
-                ...prevState,
-                email: e.target.value
-            }
-        })
-    }
-    handlePhoneChange(e) {
-        this.setState( (prevState) => {
-            return {
-                ...prevState,
-                phone: e.target.value
-            }
-        })
-    }
+    
     handleSubmit(e) {
         e.preventDefault()
         this.props.submit(this.state)
@@ -62,7 +44,7 @@ class GeneralEdit extends Component {
                         id='name' 
                         name='name'
                         value={this.state.name}
-                        onChange={this.handleNameChange}
+                        onChange={(e) => this.handleInputChange(e, 'name')}
                     />
                     </label>
 
@@ -73,7 +55,7 @@ class GeneralEdit extends Component {
                         id='email' 
                         name='email' 
                         value={this.state.email}
-                        onChange={this.handleEmailChange}
+                        onChange={(e) => this.handleInputChange(e, 'email')}
                     />
                     </label>
 
@@ -84,7 +66,7 @@ class GeneralEdit extends Component {
                         id='phone' 
                         name='phone' 
                         value={this.state.phone}
-                        onChange={this.handlePhoneChange}
+                        onChange={(e) => this.handleInputChange(e, 'phone')}
                     />
                     </label>
 
